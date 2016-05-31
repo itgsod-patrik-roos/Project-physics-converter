12837880ca9bf110a977dd1e3253ec28580a3586
itgsod-patrik-roos <patrik.roos@elev.it-gymnasiet.se>
2016-05-31 08:53:44 +0200

    Add files via upload

---
+++ test_flask_restfull/formulas.py
@@ -0,0 +1,12 @@
+from decimal import decimal
+def Coulombs_law(force,charge1,charge2,distance):
+        if force == "?":
+            1+1  = force
+
+            print force
+
+
+
+
+
+Coulombs_law("?",2,2,0.02 )
